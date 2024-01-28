31d03dac6d8efd40291ae1cc3193e695f4de62c7
Prakarsh <jhethia@gmail.com>
2024-01-28 10:41:48 +0530

    Initiated StockService

---
+++ StockService/src/main/java/com/ezcrud/StockService/StockServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ezcrud.StockService;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Log4j2
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		log.info("Stock Service Started: ");
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
